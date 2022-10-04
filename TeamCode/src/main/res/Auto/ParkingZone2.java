05f4bad2b254b568f659e5d8672c34a760f151f8
Elinora Melgarejo <96452829+ElinoraMelgarejo@users.noreply.github.com>
2022-10-04 15:01:38 -0400

    Update ParkingZone2.java

--- TeamCode/src/main/res/Auto/ParkingZone2.java
+++ TeamCode/src/main/res/Auto/ParkingZone2.java
@@ -77,7 +77,7 @@ public class RobotAutoDriveByEncoder_Linear extends LinearOpMode {
     // This is gearing DOWN for less speed and more torque.
     // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
     static final double     COUNTS_PER_MOTOR_REV    = 537.7 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 20 ;     // No External Gearing.
+    static final double     DRIVE_GEAR_REDUCTION    = 19.2 ;     // No External Gearing.
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                                       (WHEEL_DIAMETER_INCHES * Math.PI);
